abe0c4245c2b4b56bf63f92ca85e2cde848a78e5
devin <77528948+zhengdechang@users.noreply.github.com>
2024-08-13 17:19:46 +0800

    Update install-docker-compose.sh

--- install-docker-compose.sh
+++ install-docker-compose.sh
@@ -20,6 +20,6 @@ echo \
 apt-get update
 apt-get install docker-ce docker-ce-cli containerd.io -y
 usermod -aG docker $(whoami)
-curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+curl -L "https://github.com/docker/compose/releases/download/2.23.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
